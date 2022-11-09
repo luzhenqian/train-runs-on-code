827a4258ef59fe4c58b5151da69ff89391678a68
LuZhenqian <luzhenqian98@gmail.com>
2022-11-09 13:17:21 +0800

    feat: build juejin

--- scripts/build-juejin.js
+++ scripts/build-juejin.js
@@ -2,50 +2,103 @@ const fs = require('fs');
 const replaceAll = require("replaceall");
 const parse = require('node-html-parser').parse
 
+const entry = './index.html'
+const resourceLocalPath = './assets'
+const resourceOSSPath = 'https://train-runs-on-code.oss-cn-hangzhou.aliyuncs.com/assets/'
+const output = './dist'
+
 function replacePath(source, rule, replacement) {
   return replaceAll(rule, replacement, source);
 }
 
-function merge(source1, source2) {
-  return `${source1}
-  ${source2}`;
+function replaceResourcesPath(source) {
+  return replacePath(source, resourceLocalPath, resourceOSSPath)
 }
 
-const preloadJS = fs.readFileSync('./preload.js', 'utf8')
-const mainJS = fs.readFileSync('./main.js', 'utf8')
-const indexHTML = fs.readFileSync('./index.html', 'utf8')
-const stylesCSS = fs.readFileSync('./styles.css', 'utf8')
+function merge(...source) {
+  return source.join('\n')
+}
 
-const newMainJS = replacePath(mainJS, './assets/', 'https://train-runs-on-code.oss-cn-hangzhou.aliyuncs.com/assets/')
-const newHTML = replacePath(indexHTML, './assets/', 'https://train-runs-on-code.oss-cn-hangzhou.aliyuncs.com/assets/')
-const newCSS = replacePath(stylesCSS, './assets/', 'https://train-runs-on-code.oss-cn-hangzhou.aliyuncs.com/assets/')
-const newJS = merge(preloadJS, newMainJS);
-
-const root = parse(newHTML)
-const localScripts = root.querySelectorAll(`body > script`)
-Array.from(localScripts).forEach(script => {
-  script.remove()
-})
-const localLinks = root.querySelectorAll(`body > link`)
-Array.from(localLinks).forEach(link => {
-  link.remove()
-})
-
-const externalScripts = root.querySelectorAll('head > script[src^=http]')
-const externalScriptsCode = Array.from(externalScripts).map(script => {
-  return script.outerHTML
-}).join('\n')
-
-const HTMLCode = merge(externalScriptsCode, root.querySelectorAll(`body`))
-
-if (fs.existsSync('./dist')) {
-  fs.rmSync('./dist', {
-    recursive: true,
-    force: true
+function extractJS(document) {
+  const scripts = document.querySelectorAll(`script`)
+  let externalScriptCode = ''
+  const internalScripts = []
+  Array.from(scripts).forEach(script => {
+    const src = script.getAttribute('src')
+    if (!src.startsWith('http')) {
+      const source = fs.readFileSync(src, 'utf8')
+      externalScriptCode = merge(externalScriptCode, source)
+    } else {
+      internalScripts.push(script.outerHTML)
+    }
+    script.remove()
   })
+  return {
+    externalScriptCode,
+    internalScripts
+  }
 }
 
-fs.mkdirSync('./dist')
-fs.writeFileSync(`./dist/main.js`, newJS);
-fs.writeFileSync(`./dist/index.html`, HTMLCode);
-fs.writeFileSync(`./dist/styles.css`, newCSS);
+function extractCSS(document) {
+  const styles = document.querySelectorAll(`style`)
+  let externalStyleCode = ''
+  Array.from(styles).forEach(style => {
+    externalStyleCode = merge(externalStyleCode, style.outerHTML)
+    style.remove()
+  })
+
+  const links = document.querySelectorAll(`link`)
+  const internalLinks = []
+  Array.from(links).forEach(link => {
+    const href = link.getAttribute('href')
+    if (!href.startsWith('http')) {
+      externalStyleCode = merge(externalStyleCode, fs.readFileSync(href, 'utf8'))
+    } else {
+      internalLinks.push(link.outerHTML)
+    }
+    link.remove()
+  })
+  return {
+    externalStyleCode,
+    internalLinks
+  }
+}
+
+function generateCode(document) {
+  const { externalScriptCode, internalScripts } = extractJS(document)
+  const { externalStyleCode, internalLinks } = extractCSS(document)
+  const body = document.querySelector('body')
+  const html = replaceResourcesPath(merge(internalScripts.join('\n'), internalLinks.join('\n'), body))
+  const css = replaceResourcesPath(externalStyleCode)
+  const js = replaceResourcesPath(externalScriptCode)
+  return {
+    html,
+    css,
+    js
+  }
+}
+
+function rmOldDir() {
+  if (fs.existsSync(output)) {
+    fs.rmSync(output, {
+      recursive: true,
+      force: true
+    })
+  }
+}
+
+function generateFile(js, css, html) {
+  fs.mkdirSync(output)
+  fs.writeFileSync(`${output}/main.js`, js);
+  fs.writeFileSync(`${output}/styles.css`, css);
+  fs.writeFileSync(`${output}/index.html`, html);
+}
+
+function main() {
+  const root = parse(fs.readFileSync(entry, 'utf8'))
+  const { html, css, js } = generateCode(root)
+  rmOldDir()
+  generateFile(js, css, html)
+}
+
+main()
